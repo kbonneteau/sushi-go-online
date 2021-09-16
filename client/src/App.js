90fc40e6949fb244b77acf2c34e17cb8c870b066
Kayla Bonneteau <k.bonneteau@gmail.com>
2021-09-15 22:13:17 -0700

    Add initial router pages. Comment out Routes to remove issues with running code.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import './App.scss';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        Hello World
+      </div>
+      <Switch>
+        {/* <Route exact path="/" component={HomePage} /> */}
+        {/* <Route path="/game/:gameId" component={GamePage} /> */}
+        {/* <Route path="/guide" component={GuidePage} /> */}
+      </Switch>
+    </BrowserRouter>
   );
 }
 
