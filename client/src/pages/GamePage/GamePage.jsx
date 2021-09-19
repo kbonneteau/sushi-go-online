4c7a7a15d89d1f7e1c51d846401878a03ff73e60
Kayla Bonneteau <k.bonneteau@gmail.com>
2021-09-19 11:56:45 -0700

    Add main pages to pages folder.

---
+++ client/src/pages/GamePage/GamePage.jsx
@@ -0,0 +1,11 @@
+import './GamePage.scss';
+
+const GamePage = () => {
+    return (
+        <main>
+            <h1>Game Page</h1>
+        </main>
+    );
+};
+
+export default GamePage;
---
+++ client/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import './HomePage.scss';
+
+const HomePage = () => {
+    return (
+        <main>
+            <h1>Home Page</h1>
+        </main>
+    );
+};
+
+export default HomePage;
