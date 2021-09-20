cba69f706e1934302b86d80a4bbe9a4dc30077f3
Kayla Bonneteau <k.bonneteau@gmail.com>
2021-09-19 19:50:52 -0700

    Add custom names, types and colours to MenuCard component based on card props.

--- client/src/components/MenuCard/MenuCard.jsx
+++ client/src/components/MenuCard/MenuCard.jsx
@@ -1,14 +1,14 @@
 import './MenuCard.scss';
 
 // Can this be a stateful component and show a tooltip with information about the card?
-const MenuCard = () => {
+const MenuCard = ({ type, name}) => {
     return (
-        <article className="menu-card">
+        <article className={`menu-card ${name}`}>
             <header className="menu-card__dish-type">
                 {/* Can this be an h3? */}
-                type
+                {type}
             </header>
-            <h3 className="menu-card__dish-name">card name</h3>
+            <h3 className="menu-card__dish-name">{name}</h3>
             <img className="menu-card__dish-image" src="https://via.placeholder.com/100x100" alt="" />
             {/* <p className="menu-card__desc">lorem ipsum dolor</p> */}
         </article>
