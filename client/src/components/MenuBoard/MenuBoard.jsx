3f79241c60f37864f47933cd66d5b195d66b0357
Kayla Bonneteau <k.bonneteau@gmail.com>
2021-09-19 19:01:12 -0700

    Refactor data structure for menu data. Refactor menu size. Map MenuCards to menu.

--- client/src/components/MenuBoard/MenuBoard.jsx
+++ client/src/components/MenuBoard/MenuBoard.jsx
@@ -5,18 +5,40 @@ import MenuCard from '../MenuCard/MenuCard';
 
 const menu = {
     "menuType": "original",
-    "menuCards": {
-        "nigiri": "nigiri",
-        "rolls": "maki",
-        "special": ["wasabi", "chopsticks"],
-        "appetizer": ["dumpling", "tempura", "sashimi"],
-        "dessert": "pudding"
-    }
+    "menuCards": [
+        {
+            "dishType": "nigiri",
+            "dishName": ["nigiri"],
+            "image": ""
+        },
+        {
+            "dishType": "rolls",
+            "dishName": ["maki"],
+            "image": ""
+        },
+        {
+            "dishType": "special",
+            "dishName": ["wasabi", "chopsticks"],
+            "image": ""
+        },
+        {
+            "dishType": "appetizer",
+            "dishName": ["dumpling", "tempura", "sashimi"],
+            "image": ""
+        },
+        {
+            "dishType": "dessert",
+            "dishName": ["pudding"],
+            "image": ""
+        }
+    ]
 }
 
 
 class MenuBoard extends Component {
-    state = {};
+    state = {
+        cards: null
+    };
 
 
     componentDidMount() {
@@ -24,14 +46,24 @@ class MenuBoard extends Component {
         this.setState({
             cards: menu.menuCards
         })
-        console.log(Object.entries(menu.menuCards))
+        // console.log(Object.entries(menu.menuCards))
     }
     
     render() {
         console.log('render')
         return (
             <div className="menu-board">
-                <MenuCard />
+                {this.state.cards === null ? null : (
+                    this.state.cards.map(card => {
+                        console.log(card)
+                        if(card.dishType === 'nigiri') return <MenuCardNigiri />;
+                        if(card.dishName.length > 1) {
+                            return card.dishName.map(name => <MenuCard />);
+                        }
+                        return <MenuCard />;
+                    })
+                )}
+                
             </div>
         );
     };
--- client/src/components/MenuCardNigiri/MenuCardNigiri.jsx
+++ client/src/components/MenuCardNigiri/MenuCardNigiri.jsx
@@ -5,9 +5,11 @@ const MenuCardNigiri = () => {
         <article className="menu-card">
             <header className="menu-card__dish-type">
                 {/* Can this be an h3? */}
-                <p className="">nigiri</p>
+                nigiri
             </header>
-            Menu Card
+            <h3 className="menu-card__dish-name">card name</h3>
+            <img className="menu-card__dish-image" src="https://via.placeholder.com/100x100" alt="" />
+            {/* <p className="menu-card__desc">lorem ipsum dolor</p> */}
         </article>
     );
 };
