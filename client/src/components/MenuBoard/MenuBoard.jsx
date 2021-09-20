c014a3a8fa25813eade17367497df7bc5b2cfec5
Kayla Bonneteau <k.bonneteau@gmail.com>
2021-09-19 19:17:14 -0700

    Add axios get for menu. Comment out until back-end is prepared.

--- client/src/components/MenuBoard/MenuBoard.jsx
+++ client/src/components/MenuBoard/MenuBoard.jsx
@@ -1,5 +1,6 @@
 import './MenuBoard.scss';
 import { Component } from 'react';
+import axios from 'axios';
 import MenuCardNigiri from '../MenuCardNigiri/MenuCardNigiri';
 import MenuCard from '../MenuCard/MenuCard';
 
@@ -43,8 +44,11 @@ class MenuBoard extends Component {
 
     componentDidMount() {
         console.log('component did mount')
+        // axios.get(`/menu/${original}`)
+            // .then(res => console.log(res.data))
+            // .then(res => this.setState({ cards: res.data.menuCards }))
         this.setState({
-            // cards: menu.menuCards
+            cards: menu.menuCards
         })
         // console.log(Object.entries(menu.menuCards))
     }
@@ -56,14 +60,13 @@ class MenuBoard extends Component {
                 {this.state.cards === null ? null : (
                     this.state.cards.map(card => {
                         console.log(card)
-                        if(card.dishType === 'nigiri') return <MenuCardNigiri />;
+                        if(card.dishType === 'nigiri') return <MenuCardNigiri key={card.dishName} />;
                         if(card.dishName.length > 1) {
-                            return card.dishName.map(name => <MenuCard />);
+                            return card.dishName.map(name => <MenuCard key={name} type={card.dishType} name={name} />);
                         }
-                        return <MenuCard />;
+                        return <MenuCard key={card.dishName} type={card.dishType} name={card.dishName} />;
                     })
                 )}
-                
             </div>
         );
     };
