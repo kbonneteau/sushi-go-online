5b6ec7eb9acb00f1910fb5e9026c022011890077
Kayla Bonneteau <k.bonneteau@gmail.com>
2021-09-25 22:35:54 -0700

    Add logic for calculating sashimi points

--- client/src/utils/ScoreUtils.js
+++ client/src/utils/ScoreUtils.js
@@ -8,14 +8,24 @@
 //     {id: '91c99c9d-cbdd-4be2-bc60-0b6172e12fe4', card: 'maki', subType: 'maki', numberOfRolls: 2, image: {}, }
 // ]
 
+// const cardsPlayed = [
+//     {id: '96a3d5ae-fbc2-4591-a879-0557a83d69cf', card: 'wasabi', subType: 'wasabi', image: {}, value: 'Next nigiri x3'},
+//     {id: '9258c02c-aaf9-40a8-a1e1-3c22146f9631', card: 'tempura', subType: 'tempura', image: {}, value: 'x2=5'},
+//     {id: 'eff09179-fba1-4f7c-9341-53862ed7b5e6', card: 'nigiri', subType: 'squid', image: {}, value: '3'},
+//     {id: '9e6f0639-684a-4f8e-aeba-2b3fec1fde44', card: 'pudding', subType: 'pudding', image: {}, value: 'Most 6 Least -6'},
+//     {id: '239bd62a-37e3-4f5d-9c04-a6dbbe7e2e7f', card: 'maki', subType: 'maki', numberOfRolls: 1, image: {}, },
+//     {id: '9258c02c-aaf9-40a8-a1e1-3c22146f9631', card: 'tempura', subType: 'tempura', image: {}, value: 'x2=5'},
+//     {id: 'a33ada0d-1058-4c62-8667-0c6f7ccf7a97', card: 'dumpling', subType: 'dumpling', image: {}, value: '1 3 6 10 15'},
+// ]
+
 const cardsPlayed = [
-    {id: '96a3d5ae-fbc2-4591-a879-0557a83d69cf', card: 'wasabi', subType: 'wasabi', image: {}, value: 'Next nigiri x3'},
-    {id: '9258c02c-aaf9-40a8-a1e1-3c22146f9631', card: 'tempura', subType: 'tempura', image: {}, value: 'x2=5'},
-    {id: 'eff09179-fba1-4f7c-9341-53862ed7b5e6', card: 'nigiri', subType: 'squid', image: {}, value: '3'},
-    {id: '9e6f0639-684a-4f8e-aeba-2b3fec1fde44', card: 'pudding', subType: 'pudding', image: {}, value: 'Most 6 Least -6'},
-    {id: '239bd62a-37e3-4f5d-9c04-a6dbbe7e2e7f', card: 'maki', subType: 'maki', numberOfRolls: 1, image: {}, },
-    {id: '9258c02c-aaf9-40a8-a1e1-3c22146f9631', card: 'tempura', subType: 'tempura', image: {}, value: 'x2=5'},
+    {id: '0f6753a9-be5e-4814-a755-b6c63a2a8d36', card: 'maki', subType: 'maki', numberOfRolls: 3, image: {}, },
     {id: 'a33ada0d-1058-4c62-8667-0c6f7ccf7a97', card: 'dumpling', subType: 'dumpling', image: {}, value: '1 3 6 10 15'},
+    {id: 'd8c9152f-dc9b-411c-943d-7d7a9222810e', card: 'sashimi', subType: 'sashimi', image: {}, value: 'x3=10'},
+    {id: 'f679a865-d08c-4752-adf1-6c4863a15ea9', card: 'nigiri', subType: 'egg', image: {}, value: '1'},
+    {id: 'a33ada0d-1058-4c62-8667-0c6f7ccf7a97', card: 'dumpling', subType: 'dumpling', image: {}, value: '1 3 6 10 15'},
+    {id: 'd8c9152f-dc9b-411c-943d-7d7a9222810e', card: 'sashimi', subType: 'sashimi', image: {}, value: 'x3=10'},
+    {id: 'a33ada0d-1058-4c62-8667-0c6f7ccf7a97', card: 'dumpling', subType: 'dumpling', image: {}, value: '1 3 6 10 15'}
 ]
 
 const player =  {
@@ -86,8 +96,17 @@ const tempura = (cardCount) => {
     console.log('tempura score is', value)
 }
 
-const sashimi = () => {
-    console.log('hello from sashimi!')
+/**
+ * Calculates the total value of all sashimi cards played.
+ * Value designation is 3 cards = 10 points
+ * As there are only 5 sashimi cards in the deck, the highest possible score is 10
+ * 
+ * @param {number} cardCount number of tempura cards
+ * @returns {*} calculated value
+ */
+const sashimi = (cardCount) => {
+    let value = parseInt(cardCount / 3) * 10;
+    console.log('sashimi score is', value)
 }
 
 const nigiri = () => {
