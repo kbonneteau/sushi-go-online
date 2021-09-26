95a4f8661f4cc62737d53ab2a65113bfc6fba263
Kayla Bonneteau <k.bonneteau@gmail.com>
2021-09-26 00:18:55 -0700

    Add logic for calculating total maki and pudding played.

--- client/src/utils/ScoreUtils.js
+++ client/src/utils/ScoreUtils.js
@@ -8,15 +8,15 @@
 //     {id: '91c99c9d-cbdd-4be2-bc60-0b6172e12fe4', card: 'maki', subType: 'maki', numberOfRolls: 2, image: {}, }
 // ]
 
-// const cardsPlayed = [
-//     {id: '96a3d5ae-fbc2-4591-a879-0557a83d69cf', card: 'wasabi', subType: 'wasabi', image: {}, value: 'Next nigiri x3'},
-//     {id: '9258c02c-aaf9-40a8-a1e1-3c22146f9631', card: 'tempura', subType: 'tempura', image: {}, value: 'x2=5'},
-//     {id: 'eff09179-fba1-4f7c-9341-53862ed7b5e6', card: 'nigiri', subType: 'squid', image: {}, value: '3'},
-//     {id: '9e6f0639-684a-4f8e-aeba-2b3fec1fde44', card: 'pudding', subType: 'pudding', image: {}, value: 'Most 6 Least -6'},
-//     {id: '239bd62a-37e3-4f5d-9c04-a6dbbe7e2e7f', card: 'maki', subType: 'maki', numberOfRolls: 1, image: {}, },
-//     {id: '9258c02c-aaf9-40a8-a1e1-3c22146f9631', card: 'tempura', subType: 'tempura', image: {}, value: 'x2=5'},
-//     {id: 'a33ada0d-1058-4c62-8667-0c6f7ccf7a97', card: 'dumpling', subType: 'dumpling', image: {}, value: '1 3 6 10 15'},
-// ]
+const cardsPlayed = [
+    {id: '96a3d5ae-fbc2-4591-a879-0557a83d69cf', card: 'wasabi', subType: 'wasabi', image: {}, value: 'Next nigiri x3'},
+    {id: '9258c02c-aaf9-40a8-a1e1-3c22146f9631', card: 'tempura', subType: 'tempura', image: {}, value: 'x2=5'},
+    {id: 'eff09179-fba1-4f7c-9341-53862ed7b5e6', card: 'nigiri', subType: 'squid', image: {}, value: '3'},
+    {id: '9e6f0639-684a-4f8e-aeba-2b3fec1fde44', card: 'pudding', subType: 'pudding', image: {}, value: 'Most 6 Least -6'},
+    {id: '239bd62a-37e3-4f5d-9c04-a6dbbe7e2e7f', card: 'maki', subType: 'maki', numberOfRolls: 1, image: {}, },
+    {id: '9258c02c-aaf9-40a8-a1e1-3c22146f9631', card: 'tempura', subType: 'tempura', image: {}, value: 'x2=5'},
+    {id: 'a33ada0d-1058-4c62-8667-0c6f7ccf7a97', card: 'dumpling', subType: 'dumpling', image: {}, value: '1 3 6 10 15'},
+]
 
 // const cardsPlayed = [
 //     {id: '0f6753a9-be5e-4814-a755-b6c63a2a8d36', card: 'maki', subType: 'maki', numberOfRolls: 3, image: {}, },
@@ -28,17 +28,28 @@
 //     {id: 'a33ada0d-1058-4c62-8667-0c6f7ccf7a97', card: 'dumpling', subType: 'dumpling', image: {}, value: '1 3 6 10 15'}
 // ]
 
-const cardsPlayed = [
-    {id: 'eff09179-fba1-4f7c-9341-53862ed7b5e6', card: 'nigiri', subType: 'squid', image: {}, value: '3'},
-    {id: 'f679a865-d08c-4752-adf1-6c4863a15ea9', card: 'nigiri', subType: 'egg', image: {}, value: '1'},
-    {id: '3891e65a-3be8-49d7-880a-1058bf69b1f3', card: 'nigiri', subType: 'salmon', image: {}, value: '2'},
-    {id: '96a3d5ae-fbc2-4591-a879-0557a83d69cf', card: 'wasabi', subType: 'wasabi', image: {}, value: 'Next nigiri x3'},
-    {id: '96a3d5ae-fbc2-4591-a879-0557a83d69cf', card: 'wasabi', subType: 'wasabi', image: {}, value: 'Next nigiri x3'},
-    // {id: '9e6f0639-684a-4f8e-aeba-2b3fec1fde44', card: 'pudding', subType: 'pudding', image: {}, value: 'Most 6 Least -6'},
-    {id: '0f6753a9-be5e-4814-a755-b6c63a2a8d36', card: 'maki', subType: 'maki', numberOfRolls: 3, image: {}, },
-    {id: 'f679a865-d08c-4752-adf1-6c4863a15ea9', card: 'nigiri', subType: 'egg', image: {}, value: '1'},
-    // {id: 'd8c9152f-dc9b-411c-943d-7d7a9222810e', card: 'sashimi', subType: 'sashimi', image: {}, value: 'x3=10'}
-]
+// const cardsPlayed = [
+//     {id: 'eff09179-fba1-4f7c-9341-53862ed7b5e6', card: 'nigiri', subType: 'squid', image: {}, value: '3'},
+//     {id: 'f679a865-d08c-4752-adf1-6c4863a15ea9', card: 'nigiri', subType: 'egg', image: {}, value: '1'},
+//     {id: '3891e65a-3be8-49d7-880a-1058bf69b1f3', card: 'nigiri', subType: 'salmon', image: {}, value: '2'},
+//     {id: '96a3d5ae-fbc2-4591-a879-0557a83d69cf', card: 'wasabi', subType: 'wasabi', image: {}, value: 'Next nigiri x3'},
+//     {id: '96a3d5ae-fbc2-4591-a879-0557a83d69cf', card: 'wasabi', subType: 'wasabi', image: {}, value: 'Next nigiri x3'},
+//     // {id: '9e6f0639-684a-4f8e-aeba-2b3fec1fde44', card: 'pudding', subType: 'pudding', image: {}, value: 'Most 6 Least -6'},
+//     {id: '0f6753a9-be5e-4814-a755-b6c63a2a8d36', card: 'maki', subType: 'maki', numberOfRolls: 3, image: {}, },
+//     {id: 'f679a865-d08c-4752-adf1-6c4863a15ea9', card: 'nigiri', subType: 'egg', image: {}, value: '1'},
+//     // {id: 'd8c9152f-dc9b-411c-943d-7d7a9222810e', card: 'sashimi', subType: 'sashimi', image: {}, value: 'x3=10'}
+// ]
+
+// const cardsPlayed = [
+//     {id: '0f6753a9-be5e-4814-a755-b6c63a2a8d36', card: 'maki', subType: 'maki', numberOfRolls: 3, image: {}, },
+//     {id: '96a3d5ae-fbc2-4591-a879-0557a83d69cf', card: 'wasabi', subType: 'wasabi', image: {}, value: 'Next nigiri x3'},
+//     {id: 'd8c9152f-dc9b-411c-943d-7d7a9222810e', card: 'sashimi', subType: 'sashimi', image: {}, value: 'x3=10'},
+//     {id: '91c99c9d-cbdd-4be2-bc60-0b6172e12fe4', card: 'maki', subType: 'maki', numberOfRolls: 2, image: {}, },
+//     {id: '89d6dbdc-a835-42a5-b5bc-321d7f6a8f16', card: 'chopsticks', subType: 'chopsticks', image: {}, value: 'Swap for 2'},
+//     {id: 'f679a865-d08c-4752-adf1-6c4863a15ea9', card: 'nigiri', subType: 'egg', image: {}, value: '1'},
+//     {id: '91c99c9d-cbdd-4be2-bc60-0b6172e12fe4', card: 'maki', subType: 'maki', numberOfRolls: 2, image: {}, },
+//     // {id: '96a3d5ae-fbc2-4591-a879-0557a83d69cf', card: 'wasabi', subType: 'wasabi', image: {}, value: 'Next nigiri x3'}
+// ]
 
 const player =  {
     playerPosition: 4,
@@ -57,23 +68,6 @@ const countUniqueCards = playedCards => {
 
     playedCards.forEach(card => {
         const { id } = card;
-        const maki = 'maki';
-        const nigiri = 'nigiri';
-
-        // if( 
-        //     id === '239bd62a-37e3-4f5d-9c04-a6dbbe7e2e7f' ||
-        //     id === '91c99c9d-cbdd-4be2-bc60-0b6172e12fe4' ||
-        //     id === '0f6753a9-be5e-4814-a755-b6c63a2a8d36'
-        // ) {
-        //     cardTypes[maki] 
-        //         ? cardTypes[maki].count ++
-        //         : cardTypes = {
-        //             ...cardTypes,
-        //             [maki]: {
-        //                 icon: card.image.icon,
-        //                 count: 1
-        //             }
-        //         };
 
         if(id in cardTypes) {
             cardTypes[id].count++ 
@@ -88,7 +82,6 @@ const countUniqueCards = playedCards => {
         }
     })
 
-    // return cardTypes;
     return Object.values(cardTypes);
 }
 
@@ -163,6 +156,8 @@ const nigiri = (card) => {
  * Calculates the adjusted point values for wasabi played
  * Value of wasabi = 0, but multiplies the next wasabi played by 3
  * 
+ * Deduct by value of nigiri as nigiri scores will be added regardless of wasabi play
+ * 
  * @param {object} player 
  * @param {object} card 
  */
@@ -178,17 +173,36 @@ const wasabi = (player, card) => {
     console.log('wasabi score is', totalValue)
 }
 
+/** CHOPSTICKS FUNCTIONALITY NOT IMPLEMENTED. PLAYERS GET AUTO-ZERO
+ * If a player ends up with chopsticks in their final playedCards, they earn 0 points for the card.
+ * 
+ */
 const chopsticks = () => {
     value = 0;
     console.log('chopsticks score is', value)
 }
 
-const maki = () => {
-    console.log('hello from maki!')
+/**
+ * Calculates the number of rolls based on how many maki were on the card.
+ * No score awarded for maki yet
+ * @param {object} card 
+ * @returns 
+ */
+const maki = (card) => {
+    const rollCount = card.count * card.numberOfRolls;
+    console.log('maki total rolls', rollCount)
+    return rollCount;
 }
 
-const pudding = () => {
-    console.log('hello from pudding!')
+/**
+ * Returns count of pudding.
+ * No score awarded for pudding yet
+ * @param {object} card 
+ * @returns 
+ */
+const pudding = (card) => {
+    console.log('total pudding', card.count)
+    return card.count;
 }
 
 
@@ -222,19 +236,26 @@ const calculateScore = (player) => {
     const countedCardsOfEachType = countUniqueCards(player.cardsPlayed)
     // console.log(countedCardsOfEachType)
     let score;
+    let makiCards = 0;
+    let puddingCards = 0;
 
     countedCardsOfEachType.forEach(cardTypeCounted => {
         const calculator = determineCalculatorFunction(cardTypeCounted.card)
         // console.log(cardTypeCounted)
         if(cardTypeCounted.card === 'wasabi') {
-            calculator(player, cardTypeCounted)
+            return calculator(player, cardTypeCounted);
         }
-        // calculator(cardTypeCounted);
+        if(cardTypeCounted.card === 'maki'){
+            return makiCards += calculator(cardTypeCounted);
+        }
+        if(cardTypeCounted.card === 'pudding'){
+            return puddingCards += calculator(cardTypeCounted);
+        }
+        calculator(cardTypeCounted);
     });
+    console.log('maki TOTAL', makiCards)
+    console.log('pudding TOTAL', puddingCards)
 
-    // for(const cardType in countedCardsOfEachType) {
-    //     console.log(cardType)
-    // }
 };
 
 const calculateBonusPoints = (player) => {
@@ -243,31 +264,3 @@ const calculateBonusPoints = (player) => {
 
 calculateScore(player);
 
-
-
-/*
-
-Calculate total score:
-Where n = number of cards in playedCards
-
-
-Calculate all points for nigiri.
-Call wasabi within this? OR minus the value of of the nigiri found in wasabi from total points
-wasabi(n)
-
-Slice array to remove everything before wasabi?
-First nigiri found, score times 3
-Return { points: x, indexOfNigiri: (this won’t work with slice)/ ogValueOfNigiriFound: x }
-Index of nigiri + index of wasabi === index of nigiri in original array?
-chopsticks()
-
-pudding()
-
-maki([array of maki played], allPlayerPlayedHands)
-
-Count maki for each player
-Add points for applicable player who has highest number (6)
-Add point for player with second highest number (3)
-End of game calculation
-
-*/
