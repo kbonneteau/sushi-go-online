0ec2e776b82478c90c0507571d539bef2e989b58
Kayla Bonneteau <k.bonneteau@gmail.com>
2021-09-26 21:45:12 -0700

    Add initial styles for ErrorModal component.

--- client/src/components/ErrorModal/ErrorModal.jsx
+++ client/src/components/ErrorModal/ErrorModal.jsx
@@ -3,12 +3,14 @@ import './ErrorModal.scss';
 const ErrorModal = () => {
     return (
         <aside className="error-modal">
-            <img className="error-modal__image" src="https://via.placeholder.com/300x300" alt="" />
-            <h2 className="error-modal__title">Oops!</h2>
-            <p className="error-modal__message">
-               The game you tried to access has either expired or doesn't exist.
-            </p>
-            <button className="error-modal__button">Close</button>
+            <img className="error-modal__image" src="http://localhost:8080/modal-gifs/sushi-oops.gif" alt="" />
+            {/* <div className="error-modal__message-container"> */}
+                <h2 className="error-modal__title">Oops!</h2>
+                <p className="error-modal__message">
+                    The game you tried to access has either expired or doesn't exist.
+                </p>
+                <button className="error-modal__button">Play a new game?</button>
+            {/* </div> */}
         </aside>
     );
 };
