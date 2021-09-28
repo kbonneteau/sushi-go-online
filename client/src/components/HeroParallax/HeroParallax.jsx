8394cafd37988d971a5f7b5d01ab3dffa1638411
Kayla Bonneteau <k.bonneteau@gmail.com>
2021-09-27 18:28:04 -0700

    Clean up extra code in HeroParallax component

--- client/src/components/HeroParallax/HeroParallax.jsx
+++ client/src/components/HeroParallax/HeroParallax.jsx
@@ -14,113 +14,20 @@ const HeroParallax = ({ handleError }) => {
   }
 
   return (
-    // <section className="">
-    <div style={{ width: '100%', height: '100%', background: '#e99c8f' }}>
+    <div className="parallax">
       <Parallax ref={parallax} pages={2}>
-        <ParallaxLayer offset={1} speed={1} style={{ backgroundColor: '#805E73' }} />
-        <ParallaxLayer offset={2} speed={1} style={{ backgroundColor: '#87BCDE' }} />
+        <ParallaxLayer offset={1} speed={1} className="parallax__second-section" />
 
-        <ParallaxLayer
-          offset={0}
-          speed={0}
-          factor={3}
-          style={{
-            // backgroundImage: url('stars', true),
-            // backgroundSize: 'cover',
-          }}
-        />
-
-        <ParallaxLayer offset={1.3} speed={-0.3} style={{ pointerEvents: 'none' }}>
-          {/* <img src={'http://localhost:8080/images/sushi-small.png'} style={{ width: '15%', marginLeft: '70%' }} /> */}
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.1 }}>
-          {/* <img src={'http://localhost:8080/images/sushi-small.png'} style={{ display: 'block', width: '20%', marginLeft: '55%' }} /> */}
-          {/* <img src={'http://localhost:8080/images/sushi-small.png'} style={{ display: 'block', width: '10%', marginLeft: '15%' }} /> */}
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={1.75} speed={0.5} style={{ opacity: 0.1 }}>
-          {/* <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '70%' }} /> */}
-          {/* <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '40%' }} /> */}
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={1} speed={0.2} style={{ opacity: 0.2 }}>
-          {/* <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '10%' }} /> */}
-          {/* <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '75%' }} /> */}
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={1.6} speed={-0.1} style={{ opacity: 0.4 }}>
-          {/* <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '60%' }} /> */}
-          {/* <img src={url('cloud')} style={{ display: 'block', width: '25%', marginLeft: '30%' }} /> */}
-          {/* <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '80%' }} /> */}
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={2.6} speed={0.4} style={{ opacity: 0.6 }}>
-          {/* <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '5%' }} /> */}
-          {/* <img src={url('cloud')} style={{ display: 'block', width: '15%', marginLeft: '75%' }} /> */}
-        </ParallaxLayer>
-
-        {/* <ParallaxLayer
-          offset={2.5}
-          speed={-0.4}
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            pointerEvents: 'none',
-          }}>
-          <img src={url('earth')} style={{ width: '60%' }} />
-        </ParallaxLayer> */}
-
-        {/* <ParallaxLayer
-          offset={2}
-          speed={-0.3}
-          style={{
-            backgroundSize: '80%',
-            backgroundPosition: 'center',
-            backgroundImage: url('clients', true),
-          }}
-        /> */}
-
-        <ParallaxLayer
-          offset={0}
-          speed={0.1}
-        //   onClick={}
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
+        <ParallaxLayer offset={0} speed={0.1} className="parallax__parent-container">
             <HomeHero handleError={handleError} handleTutorialClick={handleTutorialClick} />
-          {/* <img src={url('server')} style={{ width: '20%' }} /> */}
         </ParallaxLayer>
 
-        <ParallaxLayer
-          offset={1}
-          speed={0.1}
-          onClick={() => parallax.current.scrollTo(0)}
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
+        <ParallaxLayer offset={1} speed={0.1} className="parallax__parent-container" onClick={() => parallax.current.scrollTo(0)}>
           <img src={url('bash')} style={{ width: '40%' }} />
         </ParallaxLayer>
 
-        {/* <ParallaxLayer
-          offset={2}
-          speed={-0}
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-          onClick={() => parallax.current.scrollTo(0)}>
-          <img src={url('clients-main')} style={{ width: '40%' }} />
-        </ParallaxLayer> */}
       </Parallax>
     </div>
-    // </section>
   )
 }
 
